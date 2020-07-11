7520e68dff51defa3288fb76e907925ab80b937b
khoi <git@khoi.io>
2020-07-11 20:28:18 +0700

    Add script to build macOS framework

--- generate-xcframework.sh
+++ generate-xcframework.sh
@@ -5,3 +5,9 @@ xcodebuild archive -scheme "Peertalk iOS" -destination="iOS Simulator" -archiveP
 xcodebuild -create-xcframework -framework /tmp/xcf/ios.xcarchive/Products/Library/Frameworks/Peertalk.framework -framework /tmp/xcf/iossimulator.xcarchive/Products/Library/Frameworks/Peertalk.framework -output ./Peertalk-iOS.xcframework
 
 echo "./Peertalk-iOS.xcframework created"
+
+xcodebuild archive -scheme "Peertalk" -destination 'platform=macOS,arch=x86_64' -archivePath /tmp/xcf/macOS.xcarchive -derivedDataPath /tmp/macOS -sdk macosx SKIP_INSTALL=NO BUILD_LIBRARIES_FOR_DISTRIBUTION=YES
+xcodebuild -create-xcframework -framework /tmp/xcf/macOS.xcarchive/Products/Library/Frameworks/Peertalk.framework -framework /tmp/xcf/macOS.xcarchive/Products/Library/Frameworks/Peertalk.framework -output ./Peertalk-macOS.xcframework
+
+
+echo "./Peertalk-macOS.xcframework created"
